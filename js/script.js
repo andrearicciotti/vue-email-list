7b7d0a7678d4c4308a94eee03d505391c7dddc8c
andrearicciotti <ricciotti.andrea1993@gmail.com>
2023-11-21 14:49:52 +0100

    add: function with axios library for get datas from boolean API, datas cycled for output 10 emails into a list of contacts in DOM

--- js/script.js
+++ js/script.js
@@ -3,8 +3,20 @@ const { createApp } = Vue;
 createApp({
     data() {
         return {
-
+            mails: []
         }
     },
-    methods:{}
+    created() {
+        for (let i = 0; i < 10; i++) {
+            axios
+                .get('https://flynn.boolean.careers/exercises/api/random/mail')
+                .then((resp) => {
+                    this.mails.push(resp.data.response);
+                    console.log(this.mails);
+                });
+        }
+    },
+    methods: {
+
+    }
 }).mount('#app');
